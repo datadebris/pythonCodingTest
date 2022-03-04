176048b3b39ba262e7d4ac09a7656b819264fa75
datadebris <smhong1123@gmail.com>
2022-03-04 18:31:30 +0900

    Baaek_2480

--- Baek_2480.py
+++ Baek_2480.py
@@ -1,7 +1,4 @@
 # 주사위 세개 입력 받기
-from tkinter.tix import MAX
-
-
 a, b, c = map(int, input().split())
 
 # 상금 저장용 변수
@@ -13,18 +10,17 @@ if a == b & b == c:
     sum = 10000 + a * 1000
 
 # 2. 같은 눈이 두개만 나올 때
-elif a == b & a != c:
+elif a == b:
     sum = 1000 + a * 100
 
-elif a == c & b != c:
+elif a == c:
     sum = 1000 + a * 100
 
-elif a != b & b == c:
+elif b == c:
     sum = 1000 + b * 100
 
 # 3. 같은 눈이 하나도 없을때
 else:
-    sum = max(a, b, c)  * 100
-
+    sum = max(a, b, c) * 100
 
 print(sum)
